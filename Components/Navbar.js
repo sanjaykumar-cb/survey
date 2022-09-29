cc396791e565ab12eca2eb2ccf0096aee21c7439
sanjay kumar <sanjay@coffeebeans.io>
2022-09-29 13:42:44 +0530

    add navbar on git

---
+++ Components/Navbar.js
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import Image from "next/image";
+import React, { useState } from "react";
+import Logo from "./Logo";
+import NavItem from "./NavItem";
+
+const MENU_LIST = [
+    { text: "Home", href: "/" },
+    { text: "About Us", href: "/about" },
+    { text: "Contact", href: "/contact" },
+    { text: "Sign In", href: "/signIn" },
+
+];
+const Navbar = () => {
+    const [navActive, setNavActive] = useState(null);
+    const [activeIdx, setActiveIdx] = useState(-1);
+    const myLoader = ({ src, width, quality }) => {
+        return `logo.png`
+    }
+
+    return (
+        <header>
+            <nav className={`nav`}>
+                <Link href={"/"}>
+                    <a>
+                        <Image loader={myLoader} src="logo.png" alt="Picture of the author" width={250} height={65}
+                        />
+                    </a>
+                </Link>
+                <div
+                    onClick={() => setNavActive(!navActive)}
+                    className={`nav__menu-bar`}
+                >
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className={`${navActive ? "active" : ""} nav__menu-list`}>
+                    {MENU_LIST.map((menu, idx) => (
+                        <div
+                            onClick={() => {
+                                setActiveIdx(idx);
+                                setNavActive(false);
+                            }}
+                            key={menu.text}
+                        >
+                            <NavItem active={activeIdx === idx} {...menu} />
+                        </div>
+                    ))}
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Navbar;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+import Contact from "./contact";
+import SignIn from "./signIn";
+
+export default function Home() {
+  return (
+
+    <div className="center">
+      <h2>Home Page</h2>
+    </div>
+  );
+}
